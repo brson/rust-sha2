c839740053eb3901e3f582ba17c183dfaea57867
Brian Anderson <banderson@mozilla.com>
2016-05-06 18:21:36 +0000

    Initial

---
+++ README.md
@@ -0,0 +1,14 @@
+SHA-2 in pure Rust.
+
+Originally from [rust-crypto]. This has the advantage of not depending
+on native components, so it should work everywhere without effort.
+
+[Documentation].
+
+[rust-crypto]: https://github.com/DaGenix/rust-crypto
+[Documentation]: https://brson.github.com/rust-sha2/sha2
+
+## License
+
+MIT/Apache-2.0
+
---
+++ lib.rs
@@ -0,0 +1,16 @@
+#![allow(dead_code)]
+extern crate rustc_serialize as serialize;
+
+#[path = "rust-crypto/src/buffer.rs"]
+pub mod buffer;
+#[path = "rust-crypto/src/digest.rs"]
+pub mod digest;
+#[path = "rust-crypto/src/sha2.rs"]
+pub mod sha2;
+#[path = "rust-crypto/src/symmetriccipher.rs"]
+pub mod symmetriccipher;
+
+#[path = "rust-crypto/src/cryptoutil.rs"]
+mod cryptoutil;
+#[path = "rust-crypto/src/simd.rs"]
+mod simd;
